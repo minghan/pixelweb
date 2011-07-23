8585c9f62d42a6ecac50844167b7322481fcda9f
Ming Han Teh <minghan@ning.com>
2011-07-22 21:14:59 -0700

    Test the bookmarklet

---
+++ pixelweb.js
@@ -0,0 +1,15 @@
+
+alert("hello");
+
+// jquery
+//
+//
+
+// var libs = ["/Users/minghan/Documents/hanworks/pixelweb/lib/crap.js"];
+
+// for (i in libs) {
+    // var script = document.createElement("script");
+    // script.src = libs[i];
+    // document.body.appendChild(script);
+// }
+
